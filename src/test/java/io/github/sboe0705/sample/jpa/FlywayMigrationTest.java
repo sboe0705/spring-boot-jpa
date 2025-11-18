d73077da28878d82fb3dec2b856301ef92af7e19
Sebastian Böhm <sboe0705@icloud.com>
2025-11-18 22:09:54 +0100

    improved Flyway migration test

--- src/test/java/io/github/sboe0705/sample/jpa/FlywayMigrationTest.java
+++ src/test/java/io/github/sboe0705/sample/jpa/FlywayMigrationTest.java
@@ -4,12 +4,16 @@ import io.github.sboe0705.sample.jpa.data.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.annotation.DirtiesContext.ClassMode.AFTER_CLASS;
 
 @SpringBootTest
 @ActiveProfiles("testdata")
+// required to clean up in-memory database containing migration steps containing (test) data
+@DirtiesContext(classMode = AFTER_CLASS)
 class FlywayMigrationTest {
 
     @Autowired
